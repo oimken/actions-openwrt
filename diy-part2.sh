6e29fa84bbaa3e440e70e7e356e9854262568255
oimken <oimken@gmail.com>
2020-09-22 00:58:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 
 # Add serverchan
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
